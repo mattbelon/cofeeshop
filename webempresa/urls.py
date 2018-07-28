974d62bc31a191e3ba095ff08254dbb3538ceafb
matt <matt.belon@gmail.com>
2018-07-28 07:16:25 -0300

    Commit message

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('core.urls')),
     path('services/', include('services.urls')),
+    path('blog/', include('blog.urls')),
+
 ]
 if settings.DEBUG:
 	from django.conf.urls.static import static
